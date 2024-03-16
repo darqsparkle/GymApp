8028f6c7cb74884571e9ac82ece3a46dd2d6cd1e
darqsparkle <phroopanvishnu.aids2021@citchennai.net>
2024-03-16 13:28:47 +0530

    Application

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ab0ab7fe06msh40a8586bd1b27fap1fa472jsn12b44f846210',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'ab0ab7fe06msh40a8586bd1b27fap1fa472jsn12b44f846210',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
